95b7c2b6653d8d25ceba82afd771f26fb3b7042e
lmodin <lisamodin@live.com>
2020-06-18 15:33:52 -0700

    Adding albums

---
+++ Albums.js
@@ -0,0 +1,139 @@
+var artistIds = [
+  "5eebdbf1bf2d490c13ff868d",
+  "5eebe7b2c451ef0e3f4c04f4",
+  "5eebe7b2c451ef0e3f4c04f5",
+  "5eebe7b2c451ef0e3f4c04f6",
+  "5eebe7b2c451ef0e3f4c04f7",
+  "5eebe7b2c451ef0e3f4c04f9",
+  "5eebe7b2c451ef0e3f4c04f8",
+  "5eebe7b2c451ef0e3f4c04fa",
+  "5eebe7b2c451ef0e3f4c04fb",
+  "5eebe7b2c451ef0e3f4c04fd",
+  "5eebe7b2c451ef0e3f4c04fc",
+  "5eebe7b2c451ef0e3f4c04fe",
+  "5eebe7b2c451ef0e3f4c04ff",
+  "5eebe7b2c451ef0e3f4c0503",
+  "5eebe7b2c451ef0e3f4c0508",
+  "5eebe7b2c451ef0e3f4c0500",
+  "5eebe7b2c451ef0e3f4c050d",
+  "5eebe7b2c451ef0e3f4c0505",
+  "5eebe7b2c451ef0e3f4c050a",
+  "5eebe7b2c451ef0e3f4c050f",
+  "5eebe7b2c451ef0e3f4c0501",
+  "5eebe7b2c451ef0e3f4c0512",
+  "5eebe7b2c451ef0e3f4c0502",
+  "5eebe7b2c451ef0e3f4c0517",
+  "5eebe7b2c451ef0e3f4c0504",
+  "5eebe7b2c451ef0e3f4c0509",
+  "5eebe7b2c451ef0e3f4c0506",
+  "5eebe7b2c451ef0e3f4c051c",
+  "5eebe7b2c451ef0e3f4c050e",
+  "5eebe7b2c451ef0e3f4c050b",
+  "5eebe7b2c451ef0e3f4c0507",
+  "5eebe7b2c451ef0e3f4c0514",
+  "5eebe7b2c451ef0e3f4c0521",
+  "5eebe7b2c451ef0e3f4c050c",
+  "5eebe7b2c451ef0e3f4c0513",
+  "5eebe7b2c451ef0e3f4c0526",
+  "5eebe7b2c451ef0e3f4c0510",
+  "5eebe7b2c451ef0e3f4c0519",
+  "5eebe7b2c451ef0e3f4c0518",
+  "5eebe7b2c451ef0e3f4c0511",
+  "5eebe7b2c451ef0e3f4c0515",
+  "5eebe7b2c451ef0e3f4c051e",
+  "5eebe7b2c451ef0e3f4c052b",
+  "5eebe7b2c451ef0e3f4c0523",
+  "5eebe7b2c451ef0e3f4c0528",
+  "5eebe7b2c451ef0e3f4c052d",
+  "5eebe7b2c451ef0e3f4c0516",
+  "5eebe7b2c451ef0e3f4c051d",
+  "5eebe7b2c451ef0e3f4c0532",
+  "5eebe7b2c451ef0e3f4c051a",
+  "5eebe7b2c451ef0e3f4c051b",
+  "5eebe7b2c451ef0e3f4c0522",
+  "5eebe7b2c451ef0e3f4c0530",
+  "5eebe7b2c451ef0e3f4c051f",
+  "5eebe7b2c451ef0e3f4c0537",
+  "5eebe7b2c451ef0e3f4c0520",
+  "5eebe7b2c451ef0e3f4c0527",
+  "5eebe7b2c451ef0e3f4c0535",
+  "5eebe7b2c451ef0e3f4c0524",
+  "5eebe7b2c451ef0e3f4c053c",
+  "5eebe7b2c451ef0e3f4c053a",
+  "5eebe7b2c451ef0e3f4c0525",
+  "5eebe7b2c451ef0e3f4c0529",
+  "5eebe7b2c451ef0e3f4c052c",
+  "5eebe7b2c451ef0e3f4c0541",
+  "5eebe7b2c451ef0e3f4c053f",
+  "5eebe7b2c451ef0e3f4c052a",
+  "5eebe7b2c451ef0e3f4c052e",
+  "5eebe7b2c451ef0e3f4c0531",
+  "5eebe7b2c451ef0e3f4c0546",
+  "5eebe7b2c451ef0e3f4c052f",
+  "5eebe7b2c451ef0e3f4c0544",
+  "5eebe7b2c451ef0e3f4c054b",
+  "5eebe7b2c451ef0e3f4c0533",
+  "5eebe7b2c451ef0e3f4c0536",
+  "5eebe7b2c451ef0e3f4c0549",
+  "5eebe7b2c451ef0e3f4c0534",
+  "5eebe7b2c451ef0e3f4c0538",
+  "5eebe7b2c451ef0e3f4c0550",
+  "5eebe7b2c451ef0e3f4c053b",
+  "5eebe7b2c451ef0e3f4c0539",
+  "5eebe7b2c451ef0e3f4c0555",
+  "5eebe7b2c451ef0e3f4c0540",
+  "5eebe7b2c451ef0e3f4c053d",
+  "5eebe7b2c451ef0e3f4c053e",
+  "5eebe7b2c451ef0e3f4c0542",
+  "5eebe7b2c451ef0e3f4c054e",
+  "5eebe7b2c451ef0e3f4c0545",
+  "5eebe7b2c451ef0e3f4c0543",
+  "5eebe7b2c451ef0e3f4c0547",
+  "5eebe7b2c451ef0e3f4c0548",
+  "5eebe7b2c451ef0e3f4c054c",
+  "5eebe7b2c451ef0e3f4c0553",
+  "5eebe7b2c451ef0e3f4c054a",
+  "5eebe7b2c451ef0e3f4c054d",
+  "5eebe7b2c451ef0e3f4c0551",
+  "5eebe7b2c451ef0e3f4c0552",
+  "5eebe7b2c451ef0e3f4c054f",
+  "5eebe7b2c451ef0e3f4c0556",
+  "5eebe7b2c451ef0e3f4c0554",
+  "5eebe7b2c451ef0e3f4c0557"
+]
+
+var albums = [ {"title": "vulputate,"}, {"title": "aliquam"}, {"title": "Mauris vestibulum, neque sed dictum"}, {"title": "amet risus. Donec"}, {"title": "tellus. Nunc lectus"}, {"title": "accumsan neque"}, {"title": "sapien, cursus in, hendrerit"}, {"title": "Donec sollicitudin adipiscing ligula. Aenean"}, {"title": "mauris blandit mattis. Cras eget"}, {"title": "arcu iaculis enim, sit"}, {"title": "est"}, {"title": "tincidunt, nunc"}, {"title": "sem, consequat nec, mollis"}, {"title": "tellus lorem eu metus."}, {"title": "libero nec"}, {"title": "ipsum"}, {"title": "a neque."}, {"title": "diam dictum"}, {"title": "arcu. Vestibulum ut"}, {"title": "vel, faucibus"}, {"title": "penatibus et magnis dis parturient"}, {"title": "dolor. Nulla semper"}, {"title": "mollis. Integer tincidunt"}, {"title": "eu"}, {"title": "auctor quis, tristique"}, {"title": "ante blandit viverra."}, {"title": "Donec elementum, lorem"}, {"title": "dui, in"}, {"title": "Morbi accumsan"}, {"title": "ipsum cursus vestibulum."}, {"title": "risus a ultricies adipiscing, enim"}, {"title": "nascetur"}, {"title": "lacinia at,"}, {"title": "fermentum fermentum"}, {"title": "ante, iaculis nec, eleifend non,"}, {"title": "non, dapibus rutrum, justo."}, {"title": "Nunc ut erat. Sed nunc"}, {"title": "condimentum"}, {"title": "massa. Mauris vestibulum,"}, {"title": "erat volutpat. Nulla dignissim. Maecenas"}, {"title": "Sed et"}, {"title": "euismod est arcu ac orci."}, {"title": "vitae aliquam eros"}, {"title": "nonummy. Fusce"}, {"title": "odio,"}, {"title": "Donec non justo."}, {"title": "Phasellus vitae mauris"}, {"title": "vitae"}, {"title": "elit. Etiam"}, {"title": "nec,"}, {"title": "ullamcorper magna. Sed eu"}, {"title": "quis turpis vitae purus gravida"}, {"title": "elit,"}, {"title": "velit"}, {"title": "eu lacus. Quisque imperdiet,"}, {"title": "magna sed dui. Fusce aliquam,"}, {"title": "gravida non, sollicitudin a,"}, {"title": "magnis"}, {"title": "lorem eu metus. In"}, {"title": "Nullam scelerisque neque sed"}, {"title": "consequat enim diam"}, {"title": "urna convallis erat, eget"}, {"title": "vehicula et,"}, {"title": "Phasellus in felis. Nulla tempor"}, {"title": "Donec feugiat"}, {"title": "penatibus et magnis"}, {"title": "hendrerit"}, {"title": "Mauris magna. Duis"}, {"title": "at"}, {"title": "imperdiet, erat nonummy ultricies"}, {"title": "scelerisque mollis. Phasellus libero"}, {"title": "ridiculus mus. Aenean eget magna."}, {"title": "magna a tortor. Nunc"}, {"title": "purus. Maecenas libero"}, {"title": "libero. Proin mi. Aliquam"}, {"title": "Nullam"}, {"title": "mi enim, condimentum"}, {"title": "Aliquam ornare, libero"}, {"title": "bibendum ullamcorper. Duis"}, {"title": "Donec porttitor tellus non"}, {"title": "a,"}, {"title": "risus. Quisque libero"}, {"title": "odio sagittis semper. Nam tempor"}, {"title": "vulputate,"}, {"title": "ac"}, {"title": "egestas. Sed pharetra, felis"}, {"title": "pede. Cras vulputate"}, {"title": "Mauris molestie"}, {"title": "elementum, dui quis accumsan convallis,"}, {"title": "amet"}, {"title": "Mauris"}, {"title": "auctor non, feugiat nec, diam."}, {"title": "risus varius orci, in consequat"}, {"title": "at, iaculis quis, pede."}, {"title": "congue turpis. In condimentum. Donec"}, {"title": "sit amet diam"}, {"title": "sed, hendrerit a, arcu."}, {"title": "dis parturient montes,"}, {"title": "et"}, {"title": "nibh. Quisque"} ];
+
+const images = [
+  "https://tinyurl.com/y922ywyh",
+  "https://tinyurl.com/yc9dd4jq",
+  "https://tinyurl.com/y8tnz963",
+  "https://tinyurl.com/y7jjtjob",
+  "https://tinyurl.com/yb46edpn",
+  "https://tinyurl.com/ya8hbpg9",
+  "https://tinyurl.com/y8thqkfd",
+  "https://tinyurl.com/yakbv7ju"
+]
+
+const type = [
+  "album", "single", "EP", "compilation"
+]
+
+let addImagesandArtists = function(images, artistIds, albums) {
+  //loop and add image
+  for (var i = 0; i < albums.length; i++) {
+    var index = Math.floor(Math.random() * images.length - 1);
+    albums[i].imageUrl = images[index]
+  }
+  //loop and add artist
+  for (var i = 0; i < albums.length; i++) {
+    var index = Math.floor(Math.random() * artistIds.length - 1);
+    albums[i].artistId = artistIds[index]
+  }
+  return albums;
+}
+
+
+addImagesandArtists(images, artistIds, albums);
+
+module.exports = albums
