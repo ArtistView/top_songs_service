a211240142b0f490d330312c6b4edc5516ba2909
lmodin <lisamodin@live.com>
2020-06-19 15:58:39 -0700

    Updating all files with database added, populated, server connecting

--- server/server.js
+++ server/server.js
@@ -3,25 +3,60 @@ const app = Express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const Promise = require('bluebird');
 
 const PORT = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(Express.static(path.join(__dirname, "../public")))
+app.use(Express.static(path.join(__dirname, "../public")));
 
-//const db = require('../../albums_service/database/db.js');
-//Promise.promisifyAll(require('mongoose'));
+const db = require('../database/database.js');
+Promise.promisifyAll(require('mongoose'));
 
 //get route for requests coming in, with songId
 app.get('/songs/:songId', (req, res) => {
-  var id = req.params.songId
+  var id = req.params.songId;
+  console.log('I got a get');
   db.Song.find({_id: id})
     .then((data) => {
       res.send(data)
     })
-  res.end();
 })
+
+app.get('/songs', (req, res) => {
+  console.log('I got a get for top 5');
+  db.getTopFive()
+    .then((data) => {
+      res.send(data)
+    })
+})
+
+// THIS WAS USED TO POPULATE THE DATA
+// const artists = require('../../albums_service/Artists.js');
+// for (var artist of artists) {
+//   var artist1 = new db.Artist(artist);
+//   artist1.save(() => {
+//     console.log('saved artists')
+//   })
+// }
+
+// const albums = require('../../albums_service/Albums.js');
+// for (var album of albums) {
+//   var album1 = new db.Album(album);
+//   album1.save(() => {
+//     console.log('saved albums')
+//   })
+// }
+
+// const songs = require('../../albums_service/Songs.js');
+// for (var song of songs) {
+//   var song1 = new db.Song(song);
+//   song1.save(() => {
+//     console.log('saved songs')
+//   })
+// }
+
 
 app.listen(PORT, (req, res) => {
   console.log('I am listening on 3000');
--- src/client/components/song.jsx
+++ src/client/components/song.jsx
@@ -1,23 +1,47 @@
 import React from 'react';
-import {BsMusicNote} from 'react-icons/bs'
-
+import {RiMusicLine} from 'react-icons/ri';
+import {RiVolumeUpLine} from 'react-icons/ri';
+import {BsPlayFill} from 'react-icons/bs';
 //Takes in a song object, and renders all components of that song
 class Song extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isPlaying: false
+      isPlaying: false,
+      songIsSelected: false
     }
     //console.log(this.props)
   }
 
-  render () {
-    return (
-      <div>
+  selectSong() {
+    this.setState({
+      songIsSelected: true
+    });
+  }
 
-        <span><BsMusicNote />{this.props.song.title}</span>
+  playSong() {
+    this.setState({
+      isPlaying: true
+    });
+  }
+
+  render () {
+    if (this.state.songIsSelected)  {
+    return (
+      <div class="songSelected">
+        <span>
+          <a onClick={playSong}><BsPlayFill /></a>
+          {this.props.song.title}
+
+        </span>
       </div>
-    )
+    )} else {
+      return (
+        <div><a class="topSong" onClick={selectSong}>
+          <span><RiMusicLine />{this.props.song.title}</span>
+        </a></div>
+      )
+    }
   }
 
 }
